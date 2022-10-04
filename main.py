843924eab81923fec1315e1b7813acd9738393b8
luisced <luis.cedillo.maldonado@gmail.com>
2022-10-04 18:36:36 -0500

    Added venv to the repo

--- main.py
+++ main.py
@@ -6,19 +6,16 @@ import sys
 from bs4 import BeautifulSoup as bs
 
 # url of the website
-URL = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
-try:
-    html = requests.get(URL).text
-    document = bs(html, 'html.parser')
-    table = document.find('table', class_='infobox vevent')
-    python_url = table.find('th', text='Website').next_sibling.a['href']
-    version = table.find(
-        'th', text='Stable release').next_sibling.strings.__next__()
-    logo_url = table.find('img')['src']
-    logo = requests.get(f'https:{logo_url}').content
-    filename = os.path.basename(logo_url)
-    with open(filename, 'wb') as file:
-        file.write(logo)
-    print(f'{python_url}, {version}, file://{os.path.abspath(filename)}')
-except requests.exceptions.ConnectionError:
-    print("You've got problems with connection.", file=sys.stderr)
+url = "https://10fastfingers.com/typing-test/spanish"
+
+# get the html of the website
+r = requests.get(url)
+
+# parse the html
+soup = bs(r.content, 'html.parser')
+
+# get the text to type
+text = soup.find('div', attrs={'id': 'row1'}).text
+
+# print the text
+print(text)
