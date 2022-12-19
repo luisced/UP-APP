0b735c2ff3851cb02b3a2c156e273fb12ff8d5ff
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-19 14:05:36 -0600

    Created funciont for creating Subject instances

--- schedule.py
+++ schedule.py
@@ -81,6 +81,7 @@ def splitScheduleSubjects(scheduleRows: list[list[str]]) -> list[Subject]:
     '''Splits the schedule subjects into a list of subjects'''
     cell = [row for row in scheduleRows]
     # Extract the data from the row
+    objects: list[Subject] = []
     for data in cell:
         day = data[1]
         start_time = data[2]
@@ -90,6 +91,7 @@ def splitScheduleSubjects(scheduleRows: list[list[str]]) -> list[Subject]:
         start_date = data[7]
         end_date = data[8]
         group = data[9]
+        classroom = data[5]
 
         # Use regex to remove the newline characters from the data
         day = re.sub(r'\n', '', day)
@@ -100,20 +102,26 @@ def splitScheduleSubjects(scheduleRows: list[list[str]]) -> list[Subject]:
         start_date = re.sub(r'\n', '', start_date)
         end_date = re.sub(r'\n', '', end_date)
         group = re.sub(r'\n', '', group)
+        classroom = re.compile(
+            r'([^/]*)$').search(re.sub(r'\n', '', classroom)).group(1).replace('Ver', '').lstrip()
 
-        # Use regex to split the data
-        day = re.split(r'\n', day)[0]
-        teacher = re.split(r'\n', teacher)[0]
-        start_date = re.split(r'\n', start_date)[0]
-        end_date = re.split(r'\n', end_date)[0]
-        group = re.split(r'\n', group)[0]
+        # Create a Subject object
+        objects.append(createSubject(day, start_time, end_time, subject,
+                                     teacher, start_date, end_date, group, classroom))
 
-        data_list = [day, start_time, end_time, subject,
-                     teacher, start_date, end_date, group]
+    # convert list of objects to list of dictionaries
+    # objects = [subject.to_dict() for subject in objects]
 
-        cell.append(data_list)
+    return objects
 
-    return cell
+
+def createSubject(day: str, start_time: datetime, end_time: datetime, subject: str, teacher: str, start_date: datetime, end_date: datetime, group: str, classroom: str):
+    '''Creates a subject object'''
+
+    subject = Subject(day=day, startTime=start_time, endTime=end_time, name=subject, teacher=teacher,
+                      startdate=start_date, enddate=end_date, group=group, classroom=classroom)
+
+    return subject
 
 
 def getScheduleContent(browser: ChromeBrowser) -> list[list[str]]:
