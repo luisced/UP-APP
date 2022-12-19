f33ae17e1be3b41881afa4f7f764ae001aa743fc
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-18 21:42:03 -0600

    Finished selenium setup

---
+++ login.py
@@ -0,0 +1,27 @@
+# selenium 4
+
+# Run selenium and chrome driver to scrape data from cloudbytes.dev
+import time
+import os.path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+# Setup chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Ensure GUI is off
+chrome_options.add_argument("--no-sandbox")
+
+# Set path to chromedriver as per your configuration
+homedir = os.path.expanduser("~")
+webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
+
+# Choose Chrome Browser
+browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+# Get page
+browser.get("https://up4u.up.edu.mx/user/auth/login")
+
+# Extract description from page and print
+description = browser.find_element(By.XPATH, "//p[contains(text(), 'UP4U')]")
--- main.py
+++ main.py
@@ -3,50 +3,51 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
-# # Set the login URL and method
-# response = ''
-# while response == '':
-#     try:
-#         response = requests.get("https://up4u.up.edu.mx/user/auth/login")
-#         # change the terminal color to green and then reset it
-#         print("\033[92mConnection established ✅ \033[0m")
-#         time.sleep(2)
-#         break
-#     except:
-#         print("Connection refused by the server..\nLet me sleep for 5 seconds\nZZzzzz...")
-#         time.sleep(5)
-#         print("Was a nice sleep, now let me continue...")
-#         continue
-# # Parse the HTML of the login response
-# soup = BeautifulSoup(response.text, 'html.parser')
 
-# # Find the login form
-# login_form = soup.find('form', {'id': 'account-login-form'})
-# if login_form is not None:
-#     print("Login form found ✅")
-#     # change the terminal color to blue and then reset it
-#     # Find the username and password form fields
-#     username_field = login_form.find('input', {'name': 'Login[username]'})
-#     password_field = login_form.find('input', {'name': 'Login[password]'})
-#     if username_field and password_field:
-#         print("Username and password fields found ✅")
-#         # Fill in the login credentials
-#         username_field['value'] = '0250009'
-#         password_field['value'] = 'Lu5571967146#'
-#         # change the terminal color to red and then reset it with loggin in
-#         print("\033[91mLogging in...\033[0m")
-#         # Find the submit button
-#         submit_button = login_form.find(
-#             'button', {'type': 'submit', 'id': 'login-button'})
-#         # print the content of the submit button
-#         if submit_button is not None:
-#             print("Submit button found ✅")
-#             print(submit_button)
-#             # Simulate a click on the submit button
-#             submit_button.click()
-#         else:
-#             print("Submit button not found ❌")
-#     else:
-#         print("Username and password fields not found ❌")
-# else:
-#     print("Login form not found ❌")
+# Set the login URL and method
+response = ''
+while response == '':
+    try:
+        response = requests.get("https://up4u.up.edu.mx/user/auth/login")
+        # change the terminal color to green and then reset it
+        print("\033[92mConnection established ✅ \033[0m")
+        time.sleep(2)
+        break
+    except:
+        print("Connection refused by the server..\nLet me sleep for 5 seconds\nZZzzzz...")
+        time.sleep(5)
+        print("Was a nice sleep, now let me continue...")
+        continue
+# Parse the HTML of the login response
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find the login form
+login_form = soup.find('form', {'id': 'account-login-form'})
+if login_form is not None:
+    print("Login form found ✅")
+    # change the terminal color to blue and then reset it
+    # Find the username and password form fields
+    username_field = login_form.find('input', {'name': 'Login[username]'})
+    password_field = login_form.find('input', {'name': 'Login[password]'})
+    if username_field and password_field:
+        print("Username and password fields found ✅")
+        # Fill in the login credentials
+        username_field['value'] = '0250009'
+        password_field['value'] = 'Lu5571967146#'
+        # change the terminal color to red and then reset it with loggin in
+        print("\033[91mLogging in...\033[0m")
+        # Find the submit button
+        submit_button = login_form.find(
+            'button', {'type': 'submit', 'id': 'login-button'})
+        # print the content of the submit button
+        if submit_button is not None:
+            print("Submit button found ✅")
+            print(submit_button)
+            # Simulate a click on the submit button
+            submit_button.click()
+        else:
+            print("Submit button not found ❌")
+    else:
+        print("Username and password fields not found ❌")
+else:
+    print("Login form not found ❌")
