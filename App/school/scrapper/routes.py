b6079dd1f7875620f5f0b883db62380660340051
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-20 00:06:11 -0600

    Finished flask setup

--- App/school/scrapper/routes.py
+++ App/school/scrapper/routes.py
@@ -2,3 +2,8 @@ from flask import Blueprint, request, jsonify, render_template
 
 
 scrapper = Blueprint('scrapper', __name__)
+
+
+@scrapper.route('/scrapp_up4u', methods=['GET', 'POST'])
+def scrapp_up4u():
+    pass
