50958fccf1a37493b0e738fa204e0672a9264cf4
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-21 21:19:20 -0600

    Finished flask setup

--- App/school/scrapper/routes.py
+++ App/school/scrapper/routes.py
@@ -4,6 +4,26 @@ from flask import Blueprint, request, jsonify, render_template
 scrapper = Blueprint('scrapper', __name__)
 
 
-@scrapper.route('/scrapp_up4u', methods=['GET', 'POST'])
-def scrapp_up4u():
-    pass
+@scrapper.route('/scrapper_up4u', methods=['GET', 'POST'])
+def scrapp_up4u() -> dict[str, str]:
+    '''
+    This endpoint returns the schedule of a student in a json format
+    '''
+    if request.method == 'GET':
+        json_data = request.get_json()
+        data: list[dict[str, str]] = []
+        response: dict[str, str] = {}
+        error, message, code = False, '', ''
+        if json_data and all(json_data.values()):
+            if ['id', 'password'] == list(json_data.keys()):
+                data.append()
+            else:
+                error, code = 'Missing fields', 4
+        else:
+            error, code = 'No data received', 5
+    else:
+        error, code = 'Invalid method', 6
+
+    response.update({'sucess': True, 'message': message, 'message': f'{data}', 'status_code': 200, 'error': None, 'code': f'{code}'} if data and data != [{}]else {
+        'sucess': False,  'message': 'Could not get content', 'status_code': 400, 'error': f'{error}', 'code': f'{code}'})
+    return jsonify(response, response['status_code'])
--- App/school/scrapper/utils.py
+++ App/school/scrapper/utils.py
@@ -4,15 +4,11 @@ from school.dashboard.utils import findScheduleLink
 from school.schedule.utils import *
 
 
-browser = ChromeBrowser().buildBrowser()
-browser.get("https://up4u.up.edu.mx/user/auth/login")
-login = login(browser)
-scheduleLink = findScheduleLink(browser)
-scheduleContent = getScheduleContent(browser)
+def extractUP4USchedule():
+    browser = ChromeBrowser().buildBrowser()
+    browser.get("https://up4u.up.edu.mx/user/auth/login")
+    login = login(browser)
+    scheduleLink = findScheduleLink(browser)
+    scheduleContent = getScheduleContent(browser)
 
-print(scheduleContent)
-
-scheduleExcel(scheduleContent)
-# with open("horario.html", "w") as f:
-#     f.write(browser.page_source)
-browser.quit()
+    print(scheduleContent)
