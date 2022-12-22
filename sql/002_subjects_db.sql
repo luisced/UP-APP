d409efea9e9814edde89685b3ca8651dca30f571
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-22 00:33:17 -0600

    Added subject table

---
+++ sql/002_subjects_db.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS `Subject`;
+
+CREATE TABLE
+    `Subject` (
+        `ID` int(11) NOT NULL AUTO_INCREMENT,
+        `Name` text NOT NULL,
+        `Teacher` text NOT NULL,
+        `Classroom` text NOT NULL,
+        `Day` text NOT NULL,
+        `StartTime` time NOT NULL,
+        `EndTime` time NOT NULL,
+        `StartDate` date NOT NULL,
+        `EndDate` date NOT NULL,
+        `Group` text NOT NULL,
+        `Status` tinyint(4) NOT NULL,
+        `CreationDate` datetime NOT NULL,
+        `LastUpdate` timestamp NOT NULL,
+        `Option` int(11) NOT NULL,
+        PRIMARY KEY (`ID`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
