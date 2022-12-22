6bd06173570396b32d70b2484318124f8c7230df
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-21 23:31:59 -0600

    Added endpoint (unfinished) for schedule

--- App/school/models.py
+++ App/school/models.py
@@ -9,15 +9,17 @@ import os.path
 class ChromeBrowser:
     """Class to create a Chrome browser instance"""
 
+    def __init__(self):
+        """Constructor method to initialize the Chrome browser instance"""
+        self.chromeOptions = Options()
+        self.chromeOptions.add_argument("--headless")  # Ensure GUI is off
+        self.chromeOptions.add_argument("--no-sandbox")
+
     def buildBrowser(self) -> webdriver:
         """Method to build a Chrome browser instance"""
-        """Constructor method to initialize the Chrome browser instance"""
-        chromeOptions = webdriver.ChromeOptions()
-        chromeOptions.add_argument("--headless")  # Ensure GUI is off
-        chromeOptions.add_argument("--no-sandbox")
-
-        browser = webdriver.Remote(
-            command_executor='http://localhost:3000',
-            options=chromeOptions
-        )
+        homedir = os.path.expanduser("~")
+        webdriverService = Service(
+            f"{homedir}/chromedriver/stable/chromedriver")
+        browser = webdriver.Chrome(
+            service=webdriverService, options=self.chromeOptions)
         return browser
--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,12 @@ ENV TZ=America/Mexico_City
 ENV FLASK_ENV development
 ENV DEBUG true
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-# RUN apt-get install -y wget
+RUN apt-get install -y wget
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+RUN apt-get -y update
+RUN apt-get install -y google-chrome-stable
+
 # RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \ 
 #     && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
 # RUN apt-get update && apt-get -y install google-chrome-stable
