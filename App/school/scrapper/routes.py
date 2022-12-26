d338582651b7c44e76658f089b26dbc925c73f3a
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-26 00:03:50 -0600

    Optimized user querys

--- App/school/scrapper/routes.py
+++ App/school/scrapper/routes.py
@@ -16,17 +16,15 @@ def scrapp_up4u() -> dict[str, str]:
         json_data = request.get_json()
         data: list[dict[str, str]] = []
         response: dict[str, str] = {}
-        error, message, code = False, '', ''
         if json_data and all(json_data.values()):
             if ['id', 'password'] == list(json_data.keys()):
                 student = Student.query.filter_by(
                     studentID=json_data['id']).first()
-                if not student:
-                    schedule = extractUP4USchedule(
-                        json_data['id'], json_data['password'])
-                    data = [subject for subject in schedule]
+                if student:
+                    data = getStudentSubjects(student)
                 else:
-                    data = [getStudentSubjects(student)]
+                    data = extractUP4USchedule(
+                        json_data['id'], json_data['password'])
                 message, code = f'Data extracted', 1
             else:
                 error, code = 'Missing fields', 2
@@ -37,4 +35,4 @@ def scrapp_up4u() -> dict[str, str]:
 
     response.update({'sucess': True, 'message': message, 'Schedule': data, 'status_code': 200, 'error': None, 'code': code} if data and data != [] and data != [None] else {
         'sucess': False,  'message': 'Could not get content', 'status_code': 400, 'error': f'{error}', 'code': code})
-    return jsonify(response, response['status_code'])
+    return jsonify(response)
